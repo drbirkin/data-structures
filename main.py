3c1962a546028d5f3fc2fc5834904184ca0ae013
David Y <d.yang@pinkelephant.com>
2023-08-28 10:47:18 -0400

    add 2d array

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python code to demonstrate the working of
 # array(), append(), insert()
-
+# https://www.geeksforgeeks.org/python-arrays/
 # importing "array" for array operations
 from array import array
 
@@ -17,6 +17,27 @@ for i in range (0, 3):
 print("\r")
 
 # using append() to insert new value at end O(n)
+
+# 1. Check Available Space:
+
+# The module checks if there is enough space in the allocated memory for the new element. If there is enough space, it can directly add the new element. If not, it needs to allocate more memory.
+# Allocate More Memory (if needed):
+
+# If there is not enough space to add the new element, the module allocates a larger chunk of memory to store the elements. This new memory chunk is typically larger than the previous one to reduce the frequency of reallocations.
+# 2. Copy Existing Elements:
+
+# The module copies the existing elements from the old memory chunk to the new one. This copying process ensures that the data is preserved during the reallocation.
+# 3. Add New Element:
+
+# The new element is added to the end of the array in the new memory chunk.
+# 4. Update Internal Metadata:
+
+# The array module updates its internal metadata, such as the size of the allocated memory and the number of elements.
+# 5. Release Old Memory (if applicable):
+
+# The old memory chunk is no longer needed and can be released to the system to free up resources.
+# TODO: show errors and bugs during those steps behind the scene
+
 int_arr.append(4)
 
 # printing appended array
@@ -70,3 +91,19 @@ print("\r")
 
 # Displaying the array
 print("Array:", int_arr)
+
+#
+# 
+# 
+# 
+
+# two-dimensional array
+two_dim_array = [array('i', [1, 2, 3]), array('i', [4, 1, 3]), array('i', [1, 2, 3]), array('i', [1, 2, 3])]
+
+for i in range(0, len(two_dim_array)):
+    # static row length to prevent memory outbound, python doesn't show error on creation
+	for j in range(0, len(two_dim_array[1])):
+		print(two_dim_array[i][j], end = ' ')
+	print('\n')
+
+print('Array 2D:', two_dim_array)
