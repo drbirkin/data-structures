4a21310f00c6ca9da16f8a35b786edbb088baddb
David Y <d.yang@pinkelephant.com>
2023-11-01 16:41:30 -0400

    add reverse array task

---
+++ practices.py
@@ -0,0 +1,30 @@
+from array import array
+
+# Write a program to reverse an array or string
+# Input  : arr[] = {1, 2, 3}
+# Output : arr[] = {3, 2, 1}
+
+# Input :  arr[] = {4, 5, 1, 2}
+# Output : arr[] = {2, 1, 5, 4}
+
+int_arr = array("i", [1, 2, 3])
+
+# printing original array
+def show_arr(arr):
+    print("The new created array is : ", end=" ")
+    for i in range(0, len(arr)):
+        print(arr[i], end=" ")
+
+    print("\r")
+    
+def reverse_arr(arr):
+    print("Reverse array")
+    for i in range(0, len(arr)):
+        if len(arr) - 1 - i == i: break
+            
+        arr[i], arr[len(arr) - 1 - i] = arr[len(arr) - 1 - i], arr[i]
+
+int_arr.reverse()
+reverse_arr(int_arr)
+
+show_arr(int_arr)
